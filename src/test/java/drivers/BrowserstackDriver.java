3340973013e3678f3f8c5a1b8ba403d35d70dcf5
nikitooooos <nikita.postnikov.00@gmail.com>
2024-01-02 18:26:43 +0300

    first commit

---
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -0,0 +1,42 @@
+package drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.aeonbits.owner.ConfigFactory;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import owner.WebDriverConfig;
+
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BrowserstackDriver implements WebDriverProvider {
+
+    static WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+
+    @Nonnull
+    @Override
+    public org.openqa.selenium.WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
+
+        caps.setCapability("browserstack.user", config.getUserName());
+        caps.setCapability("browserstack.key", config.getAccessKey());
+
+        caps.setCapability("app", config.getApp());
+
+        caps.setCapability("device", config.getDevice());
+        caps.setCapability("os_version", config.getOS());
+
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "browserstack-build-1");
+        caps.setCapability("name", "first_test");
+
+        try {
+            return new RemoteWebDriver(
+                    new URL(config.getUrl()), caps);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
